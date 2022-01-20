a0bf1e5a0a7f412e6a22e4d8f9c86b2ed943a08f
Casey Perkins <casey8.10.12csl@gmail.com>
2022-01-20 10:55:41 -0600

    First Commit1

--- assignment_Perkins.R
+++ assignment_Perkins.R
@@ -1,14 +1,31 @@
 #question 9
+
 library(tidyverse)
 library(palmerpenguins)
+
 #question 10
+
 penguins
 view(penguins)
 ggplot(data=penguins)
 ggplot(data = penguins) + 
   geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g))
+
 #question 11
+
 ggplot(data = penguins) + 
   geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, colour = species))
+ggplot(data = penguins) + 
+  geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, colour = species, shape = island))
+
+#question 13
+
+ggplot(data = penguins) + 
+  geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, colour = species, shape = island))+
+  facet_wrap(species ~ .)
+ggplot(data = penguins) + 
+  geom_point(mapping = aes(x = flipper_length_mm, y = body_mass_g, colour = species, shape = island))+
+  facet_grid(species ~ .)
+
 usethis::create_github_token()
 gitcreds::gitcreds_set()
